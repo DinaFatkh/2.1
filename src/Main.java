e4e3e7486939bcdb4a0beeddae62064738e704fa
Fatkhutdinova Dina <dinafatkh@gmail.com>
2022-02-15 21:38:39 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 2253;
+        int bonusPrice = 20;
+        int bonus = ticketPrice/bonusPrice;
+        System.out.println("Ваши мили: " + bonus);
+    }
+}
